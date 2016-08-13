864648ffa55a2669b1e9b13b1c86cd9f82546106
ThomasJohnston246 <thomas.johnston246@gmail.com>
2016-08-13 22:48:31 +1000

    Initial Commit

---
+++ javascripts/indexjavascript.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#my-menu").mmenu({
+        navbar: false,
+        extensions: ["theme-white", "border-none"]
+    });
+    var owl = $('#mainCarousel');
+    owl.owlCarousel({
+        singleItem: true,
+        pagination: false,
+    });
+    $('.rightNavB').click(function() {
+        console.log(this);
+        owl.trigger('owl.next');
+    })
+    $('.leftNavB').click(function() {
+        console.log(this);
+        owl.trigger('owl.prev');
+    })
+    var wall = new Freewall("#container");
+    wall.fitWidth();
+    window.onresize = function() {
+        wall.fitWidth();
+    }
+    var searchActivated = false;
+    $('#searchIcon').click(function() {
+        if (searchActivated == false) {
+            searchActivated = true;
+            $("#searchIcon").addClass('searchClick');
+            var inputElement =
+                '<input class="homeInput" type="text" name="fname" id="search-input" placeholder="Search...">'
+            $('.icon-linksH').prepend(inputElement);
+        } else {
+            searchActivated = false;
+            $("#searchIcon").removeClass('searchClick');
+            var inputElement =
+                '<input class="homeInput" type="text" name="fname" id="search-input" placeholder="search...">'
+            $('.homeInput').remove();
+        }
+    })
+});
